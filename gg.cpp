2e138f2066c5f7e68e74c31f94e889d4dba45160
mrtalhaali <140149420+mrtalhaali@users.noreply.github.com>
2023-09-05 18:39:55 +0000

    Helllo jee

---
+++ gg.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+class Talha
+{
+
+    public:
+        void name()
+        {
+            cout << "My name is Talha Ali " << endl ;
+            cout << "My name is Talha Ali " << endl ;
+            cout << "My name is Talha Ali " << endl ;
+        }
+};
+int main()
+{
+    cout << "skks"  << endl ;
+
+    Talha obj;
+
+    obj.name() ;
+}
